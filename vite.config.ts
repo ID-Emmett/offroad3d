da516ff464594cd5b04163e59bbeb7e6f4635cd8
ID-Emmett <1749987265@qq.com>
2024-05-03 02:48:19 +0800

    chore: setup npm link for local package development

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,32 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+
+      // !-------------------------------------link start-----------------------------------------
+      /**
+       * 链接本地源码包
+       * 1、在源码目录运行：npm link
+       * 2、当前项目目录链接：npm link @orillusion/core
+       * 3、配置 vite.config.ts 与 tsconfig.app.json
+       * 4、取消链接：npm unlink @orillusion/core 全局： npm unlink
+       * 5、重新安装编译包：npm install @orillusion/core
+       * 6、注意点：关于 physics effect 等插件，需要确定指定入口文件如 index
+       * 7、NPM包链接前信息：
+       *     "@orillusion/core": "^0.7.2",
+       *     "@orillusion/effect": "^0.1.2",
+       *     "@orillusion/particle": "^0.1.0",
+       *     "@orillusion/physics": "^0.2.2",
+       *     "@orillusion/stats": "^0.2.2",
+       */
+      "@orillusion/physics": "D:/code/orillusion/packages/physics/index",
+      "@orillusion/ammo": "D:/code/orillusion/packages/ammo/ammo",
+      "@orillusion/effect": "D:/code/orillusion/packages/effect/index",
+      "@orillusion/particle": "D:/code/orillusion/packages/particle/index",
+      "@orillusion/stats": "D:/code/orillusion/packages/stats/index",
+      '@orillusion/core': 'D:/code/orillusion/src/index',
+      "@orillusion/wasm-matrix": "D:/code/orillusion/packages/wasm-matrix",
+      // !-------------------------------------link end-------------------------------------------
     }
   }
 })
