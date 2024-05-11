a07a2a6b8753cb94a70e75a4591bc0941c481ea5
ID-Emmett <1749987265@qq.com>
2024-05-12 05:32:41 +0800

    fix: dynamic loading path and integrate global project name

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="vite/client" />
 // 声明第三方模块
 declare module '@/utils/perlin.js'
+declare const __APP_NAME__: string;
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import basicSsl from '@vitejs/plugin-basic-ssl'
-const fs = require("fs");
+
+import packageJson from './package.json';
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  define: {
+    __APP_NAME__: JSON.stringify(packageJson.name),
+  },
+  base: `/${packageJson.name}/`, // 等价 '/offroad3d/'
   server: {
     host: '0.0.0.0', // 配置IP访问
     port: 5555,
@@ -18,7 +23,6 @@ export default defineConfig({
     manifest: true
   },
   plugins: [
-    // basicSsl(),
     vue(),
   ],
   resolve: {
